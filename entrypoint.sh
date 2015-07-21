6de57b354bfb8d2bbeda1211f9efbbb74a546edf
Sameer Naik <sameer@damagehead.com>
2015-07-21 22:10:49 +0530

    entrypoint.sh: use `which`

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ chown -R ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} ${APT_CACHER_NG_LOG_DIR}
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == apt-cacher-ng || ${1} == /usr/sbin/apt-cacher-ng ]]; then
+elif [[ ${1} == apt-cacher-ng || ${1} == $(which apt-cacher-ng) ]]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
@@ -28,7 +28,7 @@ fi
 # default behaviour is to launch apt-cacher-ng
 if [[ -z ${1} ]]; then
   exec start-stop-daemon --start --chuid ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} \
-    --exec /usr/sbin/apt-cacher-ng -- -c /etc/apt-cacher-ng ${EXTRA_ARGS}
+    --exec $(which apt-cacher-ng) -- -c /etc/apt-cacher-ng ${EXTRA_ARGS}
 else
   exec "$@"
 fi
