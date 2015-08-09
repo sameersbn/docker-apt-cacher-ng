56917d4982c0ce636da04f2f4d851f1df97315b3
Sameer Naik <sameer@damagehead.com>
2015-08-09 22:14:43 +0530

    create functions for better maintenence and readability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 set -e
 
-# create pid directory
-mkdir -p /run/apt-cacher-ng
-chmod -R 0755 /run/apt-cacher-ng
-chown ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} /run/apt-cacher-ng
+create_pid_dir() {
+  mkdir -p /run/apt-cacher-ng
+  chmod -R 0755 /run/apt-cacher-ng
+  chown ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} /run/apt-cacher-ng
+}
 
-# create cache dir
-mkdir -p ${APT_CACHER_NG_CACHE_DIR}
-chmod -R 0755 ${APT_CACHER_NG_CACHE_DIR}
-chown -R ${APT_CACHER_NG_USER}:root ${APT_CACHER_NG_CACHE_DIR}
+create_cache_dir() {
+  mkdir -p ${APT_CACHER_NG_CACHE_DIR}
+  chmod -R 0755 ${APT_CACHER_NG_CACHE_DIR}
+  chown -R ${APT_CACHER_NG_USER}:root ${APT_CACHER_NG_CACHE_DIR}
+}
 
-# create log dir
-mkdir -p ${APT_CACHER_NG_LOG_DIR}
-chmod -R 0755 ${APT_CACHER_NG_LOG_DIR}
-chown -R ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} ${APT_CACHER_NG_LOG_DIR}
+create_log_dir() {
+  mkdir -p ${APT_CACHER_NG_LOG_DIR}
+  chmod -R 0755 ${APT_CACHER_NG_LOG_DIR}
+  chown -R ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} ${APT_CACHER_NG_LOG_DIR}
+}
+
+create_pid_dir
+create_cache_dir
+create_log_dir
 
 # allow arguments to be passed to apt-cacher-ng
 if [[ ${1:0:1} = '-' ]]; then
