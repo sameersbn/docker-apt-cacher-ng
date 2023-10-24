c71d319c77f74df7e9777fd6c3dab6e7566355ed
Mark Petersen <mapet@sdfi.dk>
2023-10-24 14:44:40 +0200

    bug: removed echo

--- docker/apt-proxy-detect.sh
+++ docker/apt-proxy-detect.sh
@@ -12,7 +12,6 @@ then
     exit
 fi
 
-echo nc -w1 -z "$APT_CACHER_NG_IP" "$APT_CACHER_NG_PORT"
 if nc -w1 -z "$APT_CACHER_NG_IP" "$APT_CACHER_NG_PORT" 2> /dev/null
 then
     echo -n "http://${APT_CACHER_NG_IP}:${APT_CACHER_NG_PORT}"
