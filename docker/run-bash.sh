f3791c0e82e79db23c4dd5be4eedd9fdeef914e6
Mark Petersen <mapet@sdfi.dk>
2023-10-24 14:44:57 +0200

    better name

--- docker/run-bash.sh
+++ docker/run-bash.sh
@@ -1,2 +1,2 @@
-docker run -e APT_CACHER_NG_IP=172.17.0.1 -e APT_CACHER_NG_PORT=3142 -it exsample/my_server /bin/bash
+docker run -e APT_CACHER_NG_IP=172.17.0.1 -e APT_CACHER_NG_PORT=3142 -it apt-cacher-ng/ubuntu:latest /bin/bash
 
